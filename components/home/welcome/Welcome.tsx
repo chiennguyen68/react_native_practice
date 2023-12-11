5f9da1d3b52f2bf00cb42db68373ba5f41327e4f
chiennguyen68 <chien.nguyen9c9@gmail.com>
2023-12-11 22:08:37 +0700

    update

---
+++ components/home/welcome/Welcome.tsx
@@ -0,0 +1,72 @@
+import { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlatList,
+} from "react-native";
+import { useRouter } from "expo-router";
+
+import styles from "./welcome.style";
+import { icons, SIZES } from "../../../constants";
+
+const jobTypes = ["Full-time", "Part-time", "Contractor"];
+
+const Welcome = () => {
+  const router = useRouter();
+  const [searchTerm, setSearchTerm] = useState("");
+  const [activeJobType, setActiveJobType] = useState("Full-time");
+
+  const handleClick = () => {};
+  return (
+    <View>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Xin chào, Chien</Text>
+        <Text style={styles.welcomeMessage}>Tìm việc phù hợp</Text>
+      </View>
+
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput
+            style={styles.searchInput}
+            value={searchTerm}
+            onChangeText={(text) => setSearchTerm(text)}
+            placeholder="Việc làm mong muốn ?"
+          />
+        </View>
+
+        <TouchableOpacity style={styles.searchBtn} onPress={handleClick}>
+          <Image
+            source={icons.search}
+            resizeMode="contain"
+            style={styles.searchBtnImage}
+          />
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.tabsContainer}>
+        <FlatList
+          data={jobTypes}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              style={styles.tab(activeJobType, item)}
+              onPress={() => {
+                setActiveJobType(item);
+                // router.push(`/search/${item}`);
+              }}
+            >
+              <Text style={styles.tabText(activeJobType, item)}>{item}</Text>
+            </TouchableOpacity>
+          )}
+          keyExtractor={(item) => item}
+          contentContainerStyle={{ columnGap: SIZES.small }}
+          horizontal
+        />
+      </View>
+    </View>
+  );
+};
+
+export default Welcome;
